84bc7ec3d692c5f64603c819171ccb039e0aede8
Chris Doss <christdoss2003@gmail.com>
2025-06-25 16:35:36 -0500

    Initial commit

---
+++ Tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this path depending on your file structure
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyanAccent: '#47F4FF',
+        purpleAccent: '#6E50FF',
+      },
+      fontFamily: {
+        modern: ['Poppins', 'Segoe UI', 'Roboto', 'sans-serif'],
+      },
+      animation: {
+        'star-movement-bottom': 'star-movement-bottom 6s linear infinite alternate',
+        'star-movement-top': 'star-movement-top 6s linear infinite alternate',
+      },
+      keyframes: {
+        'star-movement-bottom': {
+          '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+          '100%': { transform: 'translate(-100%, 0%)', opacity: '0' },
+        },
+        'star-movement-top': {
+          '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+          '100%': { transform: 'translate(100%, 0%)', opacity: '0' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
