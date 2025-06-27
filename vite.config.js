7ab946d31ce9cdd84eff491dccd1ef29b1797719
Chris Doss <christdoss2003@gmail.com>
2025-06-27 00:56:15 -0500

    Fixing Vite config

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      // You shouldn't typically need this if postcss.config.js is at root,
-      // but it forces the path if there's an unusual setup.
-      // Adjust 'path/to/your/postcss.config.js' if it's not at the root
-      // or if you named it something else.
-      config: './postcss.config.js', // This is often the default, but explicitly setting it can help.
+      plugins: [
+        require('autoprefixer'),
+        require('tailwindcss'),
+      ],
+      config: './postcss.config.js',
     },
   },
 });
